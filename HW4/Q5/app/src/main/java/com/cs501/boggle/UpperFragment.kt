3e9f5491b277644bc47403bd20e0b1ac72cde809
lyushang <lyushang@bu.edu>
2023-02-27 16:12:12 -0500

    Finish check user input function

--- HW4/Q5/app/src/main/java/com/cs501/boggle/UpperFragment.kt
+++ HW4/Q5/app/src/main/java/com/cs501/boggle/UpperFragment.kt
@@ -2,6 +2,8 @@ package com.cs501.boggle
 
 import android.graphics.Color
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +12,8 @@ import android.widget.GridLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import okhttp3.*
+import java.io.IOException
 
 
 /**
@@ -25,6 +29,7 @@ class UpperFragment : Fragment() {
     private var selectedLetter = mutableListOf<Int>()
     private lateinit var fragmentView: View
 
+    private var existWordDict = HashSet<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -60,11 +65,112 @@ class UpperFragment : Fragment() {
     }
 
     fun onSubmit(view: View) {
-//        TODO: Check the input word
-        println("Submit Clicked user's input is "+ inputWord?.text)
+        val word = (inputWord?.text).toString().uppercase()
+        if (containsTwoVowels(word) && hasEnoughLen(word) && isWordValid(word)) {
+            Toast.makeText(
+                activity,
+                R.string.correct_message,
+                Toast.LENGTH_SHORT
+            ).show()
+            existWordDict.add(word)
+        }
     }
 
+    private fun containsTwoVowels(word: String): Boolean {
+        val vowels = setOf('A', 'E', 'I', 'O', 'U')
+        var count = 0
+
+        for (c in word) {
+            if (c in vowels) {
+                count++
+                if (count >= 2) {
+                    return true
+                }
+            }
+        }
+
+        Toast.makeText(
+            activity,
+            R.string.not_enough_vowels_incorrect_message,
+            Toast.LENGTH_SHORT
+        ).show()
+
+        resetBoard()
+
+        return false
+    }
+
+    private fun hasEnoughLen(word: String): Boolean {
+        if (word.length >= 4) {
+            return true
+        }
+
+        Toast.makeText(
+            activity,
+            R.string.not_enough_length_incorrect_message,
+            Toast.LENGTH_SHORT
+        ).show()
+
+        resetBoard()
+
+        return false
+    }
+
+    private fun isWordValid(word: String): Boolean {
+        if (existWordDict.contains(word)) {
+            Toast.makeText(
+                activity,
+                R.string.word_exist_incorrect_message,
+                Toast.LENGTH_SHORT
+            ).show()
+
+            resetBoard()
+
+            return false
+        }
+
+        val policy = ThreadPolicy.Builder().permitAll().build()
+
+        StrictMode.setThreadPolicy(policy)
+
+        val url = "https://raw.githubusercontent.com/dwyl/english-words/master/words.txt"
+        val wordList = downloadWordList(url).split("\n").toSet()
+        if (word !in wordList) {
+            Toast.makeText(
+                activity,
+                R.string.word_not_valid_incorrect_message,
+                Toast.LENGTH_SHORT
+            ).show()
+
+            resetBoard()
+
+            return false
+        }
+
+        return true
+    }
+
+    fun downloadWordList(url: String): String {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        val response = client.newCall(request).execute()
+        if (!response.isSuccessful) {
+            throw IOException("Unexpected code $response")
+        }
+
+        return response.body?.string() ?: ""
+    }
+
+
+
     fun onClear(view: View) {
+        resetBoard()
+    }
+
+    fun resetBoard() {
         for (id in selectedLetter) {
             fragmentView.findViewById<Button>(id).setBackgroundColor(resources.getColor(R.color.purple_500))
             fragmentView.findViewById<Button>(id).isEnabled = true
@@ -85,7 +191,7 @@ class UpperFragment : Fragment() {
         } else {
             Toast.makeText(
                 activity,
-                R.string.invalidInput,
+                R.string.not_adjacent_click_error_message,
                 Toast.LENGTH_SHORT
             ).show()
         }
